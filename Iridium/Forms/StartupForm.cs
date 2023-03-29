29f43ccb43910bca06c31e9cafb218da3ed68300
vkouvelis <vkouvelis@outlook.com>
2023-03-30 00:39:48 +0300

    Solution push

---
+++ Iridium/Forms/StartupForm.cs
@@ -0,0 +1,11 @@
+namespace Iridium.Forms {
+    public partial class StartupForm : Form {
+        public StartupForm() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+    }
+}
